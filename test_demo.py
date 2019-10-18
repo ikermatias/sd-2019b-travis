cfcdebdc890db47004127c13e32fa604e07ad799
Joan Sebastián García Delgado <josegar0218@gmail.com>
2019-10-18 00:05:32 -0500

    Add test 2+1 (#1)
    * Add test
    
    * Fix test

--- test_demo.py
+++ test_demo.py
@@ -6,8 +6,8 @@ def test_add():
 # WHEN I invoke the method
 # THEN I get the addition of two numbers
     add_result = demo.add(3,4)
-    add_result2 = demo.add(1,1)
+    add_result2 = demo.add(2,1)
     assert add_result == 7
-    assert add_result == 11
+    assert add_result2 == 3
 
 
